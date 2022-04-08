14683e5311d882f3775977177c09d85b82ab4781
Anand Kulkarni <learncodingwithandy@gmail.com>
2022-04-08 09:23:29 +0530

    added dependencies

---
+++ src/main/java/com/micoservice/controllers/ProductController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.micoservice.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author AKulkarni17
+ */
+
+@RestController
+public class ProductController {
+    
+}
